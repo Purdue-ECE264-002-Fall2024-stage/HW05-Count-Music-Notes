655ca3cc51d1cb3f0943114d2634db232aede01a
Poulomi Pradhan <111753360+PoulomiPradhan@users.noreply.github.com>
2024-08-29 19:12:18 -0400

    Update hw05.c

--- hw05.c
+++ hw05.c
@@ -11,9 +11,26 @@ int countNotes(char * filename, double pitch){
 
 // Open the CSV file for reading
 //Check if the file was opened successfully
+////MAXLENGTH is maximum number if lines from CSV
 //store each line of the file
 // Read the header line to skip it
 //count occurance of each pitch   
 //consider using sscanf
+FILE *file = fopen(filename, "r");
+char line[MAXLENGTH];
+int count = 0;
+fgets(line, sizeof(line), file);
+while (fgets(line, sizeof(line), file)) {
+        double currentPitch;
+        sscanf(line, "%*[^;];%*[^;];%lf", &currentPitch);
+
+        // Compare with the target pitch
+        if (currentPitch == pitch) {
+            count++;
+        }
+    }
+
+    return count;
+
 
 }
