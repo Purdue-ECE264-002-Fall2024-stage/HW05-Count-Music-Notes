81ff2400dbb2d8ecb7a239d6ef085a05236d1da0
Poulomi Pradhan <111753360+PoulomiPradhan@users.noreply.github.com>
2024-08-29 14:41:38 -0400

    Update main.c

--- main.c
+++ main.c
@@ -12,13 +12,13 @@ if (argc != 2)
   {
     return EXIT_FAILURE;
   }
-int pitches[] = {67.0, 55.0, 43.0, 63.0, 51.0, 39.0, 65.0, 53.0, 41.0, 62.0, 50.0, 38.0};
+double pitches[] = {67.0, 55.0, 43.0, 63.0, 51.0, 39.0, 65.0, 53.0, 41.0, 62.0, 50.0, 38.0};
 int numPitches = sizeof(pitches) / sizeof(pitches[0]);
 
 for (int i = 0; i < numPitches; i++) {        
 
         int count = countNotes(argv[1], pitches[i]);
-        printf("Pitch %d : %d\n", pitches[i], count);
+        printf("Pitch %.1f : %d\n", pitches[i], count);
     }
 
     return EXIT_SUCCESS;
