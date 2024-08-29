617dae7ec7ca39c09b55b4d05575186d520d0fd6
Poulomi Pradhan <111753360+PoulomiPradhan@users.noreply.github.com>
2024-08-29 13:09:38 -0400

    Update main.c

--- main.c
+++ main.c
@@ -8,25 +8,18 @@
 #include "hw05.h"
 
 int main(int argc, char * * argv){
-
-// read input file
 if (argc != 2)
   {
     return EXIT_FAILURE;
   }
-//const char *filename = argv[1];
-//FILE *file = fopen(filename, "r");
 int pitches[] = {67.0, 55.0, 43.0, 63.0, 51.0, 39.0, 65.0, 53.0, 41.0, 62.0, 50.0, 38.0};
 int numPitches = sizeof(pitches) / sizeof(pitches[0]);
 
-for (int i = 0; i < numPitches; i++) {
-        
-        //fseek(file, 0, SEEK_SET);
+for (int i = 0; i < numPitches; i++) {        
 
         int count = countNotes(argv[1], pitches[i]);
         printf("Pitch %d : %d\n", pitches[i], count);
     }
 
-    //fclose(file);
     return EXIT_SUCCESS;
 }
